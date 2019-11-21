d9a370ba4004bfd156729d639786881593d0ffdf
Sebastian Frelle Koch <sebastian.frelle@gmail.com>
2019-11-21 16:41:01 +0100

    added basic testing

--- my_tiny_lib/lib/my_tiny_lib.ex
+++ my_tiny_lib/lib/my_tiny_lib.ex
@@ -1,18 +1,12 @@
 defmodule MyTinyLib do
-  @moduledoc """
-  Documentation for MyTinyLib.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> MyTinyLib.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def greeting_for_user(name, age, language \\ :english) do
+    case language do
+      :english ->
+        "Hi, #{name}. Did you know that your age is #{age}?"
+      :danish ->
+        "Hej, #{name}. Vidste du, at din alder er #{age}?"
+      _ ->
+        "Don't know what you're saying."
+    end
   end
 end
--- my_tiny_lib/test/my_tiny_lib_test.exs
+++ my_tiny_lib/test/my_tiny_lib_test.exs
@@ -1,8 +1,18 @@
 defmodule MyTinyLibTest do
   use ExUnit.Case
-  doctest MyTinyLib
+  # doctest MyTinyLib
 
-  test "greets the world" do
-    assert MyTinyLib.hello() == :world
+  test "greets a friend" do
+    assert MyTinyLib.greeting_for_user("Sebastian", 25) == "Hi, Sebastian. Did you know that your age is 25?"
+    assert MyTinyLib.greeting_for_user("Simon", 19, :danish) == "Hej, Simon. Vidste du, at din alder er 19?"
+    assert MyTinyLib.greeting_for_user("Frederik", 30, :greek) == "Don't know what you're saying."
+  end
+
+  test "greets a baby" do
+    assert MyTinyLib.greeting_for_user("Sebastian", 0) == "Hi, Sebastian. Did you know that your age is 0?"
+  end
+
+  test "greets a negatively-aged person" do
+    assert MyTinyLib.greeting_for_user("Naitsabes", -23) == "Hi, Naitsabes. Did you know that your age is -23?"
   end
 end
